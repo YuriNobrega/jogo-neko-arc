6fb74dfcd19aa967fd447c327028623125e19e81
Yuri José Nobrega de Queiroz <89605059+YuriNobrega@users.noreply.github.com>
2024-09-02 21:12:12 -0400

    Update music

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 const neko = document.querySelector('.neko');
 const pipe = document.querySelector('.pipe');
-const nya = new Audio ('./sounds/urunya.mp3');
-const hit = new Audio ('./sounds/nekohit.wav');
-
-nya.play();
+const nya = new Audio('./sounds/urunya.mp3');
+const hit = new Audio('./sounds/nekohit.wav');
+const music = new Audio('./sounds/meow.mp3');
 
 const jump = () => {
 
-   
+
     neko.classList.add('jump');
 
     setTimeout(() => {
@@ -18,18 +17,27 @@ const jump = () => {
 
 }
 
+nya.play();
+
 const loop = setInterval(() => {
 
 
+
+    music.loop = true;
+    music.play().catch(error => {
+        console.error('Não foi possível tocar a música de fundo:', error)
+    });
+
+
     const pipePosition = pipe.offsetLeft;
-    const nekoPosition = +window.getComputedStyle(neko).bottom.replace('px','');
+    const nekoPosition = +window.getComputedStyle(neko).bottom.replace('px', '');
 
     console.log(nekoPosition);
-    
 
-    if (pipePosition <= 120 && pipePosition > 0 && nekoPosition < 80 ) {
 
-       
+    if (pipePosition <= 120 && pipePosition > 0 && nekoPosition < 80) {
+
+
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
@@ -40,7 +48,7 @@ const loop = setInterval(() => {
         neko.style.width = '140px';
 
         hit.play();
-    
+
 
         clearInterval(loop);
 
